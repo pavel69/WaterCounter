ae2af3ccb0ffc81cc92487aed56c900a9a616c0c
Pavel <fateev.pavel@gmail.com>
2013-10-08 23:54:49 +0400

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//= require twitter/bootstrap
+
+// Or peek any of them yourself
+//= require twitter/bootstrap/transition
+//= require twitter/bootstrap/alert
+//= require twitter/bootstrap/modal
+//= require twitter/bootstrap/dropdown
+//= require twitter/bootstrap/tab
+//= require twitter/bootstrap/tooltip
+//= require twitter/bootstrap/popover
+//= require twitter/bootstrap/button
+//= require twitter/bootstrap/collapse
