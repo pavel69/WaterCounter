974c2d57b1a91ac6ada2cf5fccad6f885c4756c6
Pavel <fateev.pavel@gmail.com>
2013-10-21 09:10:50 +0400

    new root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 WaterCounter::Application.routes.draw do
   resources :counters
 
+  root 'counters#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
