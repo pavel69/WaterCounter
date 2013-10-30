f73e242fb972548888fec3e04a6244c3b2ce3429
Pavel <fateev.pavel@gmail.com>
2013-10-30 11:32:56 +0400

    правильная миграция

--- db/migrate/20131018052628_calculate_consumption.rb
+++ db/migrate/20131018052628_calculate_consumption.rb
@@ -1,20 +1,12 @@
 class CalculateConsumption < ActiveRecord::Migration
   def up
-    Counter.all.each do |counter|
-      x = counter.find_prev_record
+    Counter.all.order(:date).each do |counter|
 
-      counter.update_attributes(prev_id: x.id, warm_consumption: (counter.warm - x.warm), cold_consumption: (counter.cold - x.cold)) if !x.nil?
+      counter.save(validate: false) if counter.recalc
     end
-
-    Counter.all.each do |counter|
-      x = counter.prev_counter
-
-      counter.update_attributes(prev_warm_consumption: x.warm_consumption, prev_cold_consumption: x.cold_consumption) if !x.nil?
-    end
-
   end
 
   def down
-    Counter.update_all 'prev_id = null, warm_consumption = null, cold_consumption = null, prev_warm_consumption = null, prev_cold_consumption = null'
+    Counter.where('id > 1').update_all('prev_id = null, warm_consumption = null, cold_consumption = null, prev_warm_consumption = null, prev_cold_consumption = null')
   end
 end
